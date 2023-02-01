fd3b5ab9a60820110c163454e57105fbd51eeb41
snehau259 <snehau259@gmail.com>
2023-02-01 22:20:51 +0530

    [Sneha] initial commit

---
+++ src/main/java/com/example/udemyin28minutes/springBootMaven/SpringBootMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemyin28minutes.springBootMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenApplication.class, args);
+	}
+
+}
