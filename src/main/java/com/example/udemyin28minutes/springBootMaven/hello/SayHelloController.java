78ba2254ee856ada2bb52d2b2b67e91604ae530d
snehau259 <snehau259@gmail.com>
2023-02-06 11:19:30 +0530

    [Sneha] MVC controller using ResponseBody & Controller

---
+++ src/main/java/com/example/udemyin28minutes/springBootMaven/hello/SayHelloController.java
@@ -0,0 +1,17 @@
+package com.example.udemyin28minutes.springBootMaven.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class SayHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello()
+    {
+        return "Hello, what are you learning today";
+    }
+}
