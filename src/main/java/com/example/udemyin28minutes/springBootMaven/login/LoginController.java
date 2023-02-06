3ec696294c93e1794e0e98f1bc9de1a8a6b5f767
snehau259 <snehau259@gmail.com>
2023-02-06 16:48:44 +0530

    [Sneha] add. welcome page

--- src/main/java/com/example/udemyin28minutes/springBootMaven/login/LoginController.java
+++ src/main/java/com/example/udemyin28minutes/springBootMaven/login/LoginController.java
@@ -1,13 +1,22 @@
 package com.example.udemyin28minutes.springBootMaven.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class LoginController {
-    @RequestMapping("login")
+    @RequestMapping(value="login",method = RequestMethod.GET)
     public String goToLoginPage() {
         return "LoginController";
+    }
+
+    @RequestMapping(value="login",method = RequestMethod.POST)
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+        model.put("name",name);
+        return "Welcome";
     }
 }
