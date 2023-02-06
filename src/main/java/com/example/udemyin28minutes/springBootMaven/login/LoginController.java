14d2b9cef14ed3957b8e5c46b7a302ab87b41d69
snehau259 <snehau259@gmail.com>
2023-02-06 16:24:04 +0530

    [Sneha] add. login form

--- src/main/java/com/example/udemyin28minutes/springBootMaven/login/LoginController.java
+++ src/main/java/com/example/udemyin28minutes/springBootMaven/login/LoginController.java
@@ -1,15 +1,13 @@
 package com.example.udemyin28minutes.springBootMaven.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginController {
     @RequestMapping("login")
-    public String goToLoginPage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
+    public String goToLoginPage() {
         return "LoginController";
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/LoginController.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/LoginController.jsp
@@ -1,8 +1,7 @@
-<html>
-	<head>
-		<title> Login Please!</title>
-	</head>
-	<body>
-		Welcome to Login Page ${name}
-	</body>
-</html>
+<title>Login Page</title>
+		Welcome to the login page!
+		<form method="post">
+			Name: <input type="text" name="name">
+			Password: <input type="password" name="password">
+			<input type="submit">
+		</form>
